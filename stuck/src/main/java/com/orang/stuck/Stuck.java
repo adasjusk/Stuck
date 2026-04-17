010b2cb844055bfed2dde194166a6c1e7e1d8527
Adas <adasjusk@oranges.lt>
2026-04-17 22:01:26 +0300

    8.0

--- stuck/src/main/java/com/orang/stuck/Stuck.java
+++ stuck/src/main/java/com/orang/stuck/Stuck.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 import java.util.UUID;
-
 public class Stuck extends JavaPlugin {
     private final Map<UUID, Long> cooldowns = new HashMap<>();
     private final Random random = new Random();
